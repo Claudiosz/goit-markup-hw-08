e81fc72c3339d9299f158a03c95d60054f0ec704
Vladyslav Zavhorodnijj <vlad.zzavgorodnijj@gmail.com>
2022-11-29 18:56:47 +0200

    ю

--- js/menu.js
+++ js/menu.js
@@ -8,12 +8,13 @@
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
+  }
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  };
+  //   const scrollLockMethod = !isMenuOpen
+  //     ? 'disableBodyScroll'
+  //     : 'enableBodyScroll';
+  //   bodyScrollLock[scrollLockMethod](document.body);
+  // };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
@@ -23,6 +24,5 @@
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
